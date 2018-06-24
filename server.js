87c8a0b60f75a4170c65cff105bf7f26cf1226bb
Igor Mihailov <idor_m@yahoo.com>
2018-06-24 09:13:03 +0100

    add url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-tetrahedral-74008',
     user : 'postgres',
     password : 'california',
     database : 'smart_brain'
